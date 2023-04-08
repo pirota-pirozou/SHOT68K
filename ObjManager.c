34dc66c273b0b002692296f4ac9250807bc13723
Pirota Pirozou <tm_rev@hotmail.com>
2023-04-08 20:29:38 +0900

    スプライト表示座標　中央に補正

--- ObjManager.c
+++ ObjManager.c
@@ -8,6 +8,7 @@ static pSObj g_pObj = (pSObj)-1;             // オブジェクトのポインタ配列
 
 // プロトタイプ宣言
 static volatile void ObjFunc_Null(pSObj pObj);
+static void ObjFunc_Draw(pSObj pObj);
 
 /////////////////////////////////
 // ＯＢＪマネージャー自体の初期化
@@ -107,33 +108,43 @@ pSObj ObjManager_Make(int id, int x, int y)
     pObj->y = y;
     pObj->pat = 0;
     pObj->Update = ObjFunc_Null;
-    pObj->Draw = ObjFunc_Null;
 
     switch (id)
     {
         // プレイヤー
     case OBJ_ID_PLAYER:
+        pObj->pat = 0x0140;         // 自機パターン
+        pObj->Draw = ObjFunc_Draw;  // 描画標準
         break;
         // プレイヤーの弾
     case OBJ_ID_PBULLET:
+        pObj->pat = 0x0141;         // 自機ショットパターン
+        pObj->Draw = ObjFunc_Draw;  // 描画標準
         break;
         // 敵の弾
     case OBJ_ID_EBULLET:
+        pObj->pat = 0x0142;         // 敵共通ショットパターン
+        pObj->Draw = ObjFunc_Draw;  // 描画標準
         break;
         // 敵１
     case OBJ_ID_ENEMY1:
+        pObj->Draw = ObjFunc_Draw;  // 描画標準
         break;
         // 敵２
     case OBJ_ID_ENEMY2:
+        pObj->Draw = ObjFunc_Draw;  // 描画標準
         break;
         // 敵３
     case OBJ_ID_ENEMY3:
+        pObj->Draw = ObjFunc_Draw;  // 描画標準
         break;
         // 敵の爆発
     case OBJ_ID_EEFFECT:
+        pObj->Draw = ObjFunc_Draw;  // 描画標準
         break;
         // プレイヤーの爆発
     case OBJ_ID_PEFFECT:
+        pObj->Draw = ObjFunc_Draw;  // 描画標準
         break;
 
         // ここには来ないはず
@@ -167,4 +178,23 @@ void ObjManager_Destroy(pSObj pObj)
 /////////////////////////////////
 static volatile void ObjFunc_Null(pSObj pObj)
 {
+}
+
+/////////////////////////////////
+// スプライトの描画（デフォルト）
+/////////////////////////////////
+static void ObjFunc_Draw(pSObj pObj)
+{
+    // スプライト表示 X,Y 座標が16,16 ずつずれているので注意
+    // スプライトの中心座標 8,8 が、引数座標の0,0 になるように補正
+    int x = pObj->x + 16 - 8;
+    int y = pObj->y + 16 - 8;
+
+    CM_sp_set(pObj->plane, x, y, pObj->pat, pObj->plane);
+//					*ｄ１＝スプライトプレーン番号（０～１２７）
+//					*ｄ２＝ｘ座標
+//					*ｄ３＝ｙ座標
+//					*ｄ４＝パターンコード %HR_VR_X_X_COLR_SPATSPAT : 下位8bit:パターン / 8-12bit:カラー
+//					*ｄ５＝プライオリティ %0000_0000_0000_0_0_PR : 00 表示しない / 01 BG0>BG1>SP / 10 BG0>SP>BG1 / 11 SP>BG0>BG1
+
 }
