e3a66abfdf7b4ba806be820ef1a95a3ed2952598
Pirota Pirozou <tm_rev@hotmail.com>
2023-04-10 23:42:20 +0900

    タイトル画面の体裁を整える

--- SceneTitle.c
+++ SceneTitle.c
@@ -8,37 +8,47 @@
 extern int score;                  // スコア
 extern int hiscore;                // ハイスコア
 
+static int cou;                    // カウンタ
+
 // タイトルシーン　初期化
 void Title_Init(void)
 {
 //    printf("Title_Init() ");
     CM_sprite_clr();            // スプライトクリア
     BGTEXTCL(1, 0x0100);	    // BGTEXT1 CLR
+    CM_bg_puts("<SAMPLE GAME:SHOT68K>", 4,  6, 1);
+    CM_bg_puts("PROGRAMMED BY PIROTA.", 4, 29, 1);
+
+    cou = 0;
 }
 
 // タイトルシーン　更新
 void Title_Update(void)
 {
 //    printf(" TITLE_Update() ");
-    CM_bg_puts("TITLE_UPDATE()", 0, 1, 1);
+//    CM_bg_puts("TITLE_UPDATE()", 0, 1, 1);
     // ゲームパッドの値を取得
     unsigned short pad = GamePadManager_GetPad();
     unsigned short pad_trg = GamePadManager_GetTrigger();
 
-    if (pad_trg & PAD_A)
+    if ( (pad_trg & (PAD_A|PAD_B)) ||
+         ((pad_trg & PAD_START) == PAD_START) )
     {
-        // Aボタンでゲームシーンへ遷移
+        // A,B,STARTボタンでゲームシーンへ遷移
         SceneManager_ChangeScene(SCENE_ID_GAME);
     }
+
+    // カウンタをインクリメント
+    cou++;
 }
 
 // タイトルシーン　描画
 void Title_Draw(void)
 {
 //    printf("Title_Draw()\n");
-    CM_bg_puts("TITLE_DRAW()", 0, 2, 1);
+//    CM_bg_puts("TITLE_DRAW()", 0, 2, 1);
 
-    CM_sp_set(0, 50, 50, 0x0140, 3);
+//    CM_sp_set(0, 50, 50, 0x0140, 3);
 //					*ｄ１＝スプライトプレーン番号（０～１２７）
 //					*ｄ２＝ｘ座標
 //					*ｄ３＝ｙ座標
@@ -51,7 +61,15 @@ void Title_Draw(void)
 void Title_VSync(void)
 {
 //    printf(" Title_VSync()\n");
-    CM_bg_puts("TITLE_VSYNC()", 0, 3, 1);
+    if ((cou & 32)==0)
+    {
+        CM_bg_puts("PRESS BUTTON TO START.", 4, 16, 1);
+    }
+    else
+    {
+        CM_bg_puts("                      ", 4, 16, 1);
+    }
+
 }
 
 // タイトルシーン　クリア（終了）
